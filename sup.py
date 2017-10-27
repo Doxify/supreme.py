00f5b8fd251c0f233301f14a576fd970e9f9a0cb
Andrei Georgescu <admin@hydrapvp.net>
2017-10-26 23:23:57 -0700

    Add files via upload

---
+++ sup.py
@@ -0,0 +1,13 @@
+from sup_functions import *
+from essentials import *
+
+def main():
+    print color('info') + '----------------------------------------'
+    print color('error') + ' Sup.py' + color('success') + ' "Supreme bot written in Python"'
+    print color('lightgray') + ' Author:'+ color('info') + ' Andrei Georgescu / @vndrei'
+    print color('info') + '----------------------------------------'
+    print color('info') + '  1. Information' + color('info') + ' (/mobile_stock info)'
+    print('')
+    choice = input('')
+    
+main()
---
+++ sup_functions.py
@@ -0,0 +1,66 @@
+import urllib2
+import json
+from essentials import *
+
+mobile_stock_url = 'https://www.supremenewyork.com/mobile_stock.json'
+
+def session(url):
+    user_agent = 'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko)'
+    session.request = urllib2.Request(url, headers = {'User-Agent' : user_agent})
+        
+def get_release_info():
+    session(mobile_stock_url)
+    response = urllib2.urlopen(session.request).read()
+    response_json = json.loads(str(response))
+    release_week = response_json['release_week']
+    release_date = response_json['release_date']
+    last_update = response_json['last_mobile_api_update']
+    
+    logger('info','Release week:' + str(release_week))
+    logger('info','Release date:' + str(release_date))
+    logger('info', 'Last mobile api update:' + str(last_update))
+            
+def get_item_info(keyword, category, color, size):
+    session(mobile_stock_url)
+    response = urllib2.urlopen(session.request).read()
+    response_json = json.loads(str(response))
+    logger('info', 'Looking up details for "' + keyword + '" in "' + category + '"')
+    
+    for x in response_json['products_and_categories'][category]:
+        if keyword in x['name'] or keyword == x['name']:
+            name = x['name']
+            item_id = x['id']
+            
+            if item_id != '':
+                session('https://www.supremenewyork.com/shop/' + str(item_id) + '.json')
+                response = urllib2.urlopen(session.request).read()
+                response_json = json.loads(str(response))
+                
+                for x in response_json['styles']:
+                    if x['name'] == color:
+                        style_id = x['id']
+                        
+                        for x in x['sizes']:
+                            if x['name'] == size:
+                                size_id = x['id']
+                                in_stock = x['stock_level']
+                                print
+                                logger('info', 'Name: ' + str(name))
+                                logger('info', 'Item ID: ' + str(item_id))
+                                logger('info', 'Color: ' + str(color))
+                                logger('info', 'Style ID: ' + str(style_id))
+                                logger('info', 'Size ID: ' + str(size_id))
+                                if in_stock == 1:
+                                    logger('success', 'Item is in stock')
+                                else:
+                                    logger('error', 'Item is out of stock')
+                                return
+                    else:
+                        logger('error', 'Color not found')
+                        return
+        else:
+            logger('error', 'Keyword not found')
+            return
+    
+#keyword, category, color, size                                
+get_item_info('Pullover', 'new', 'Yellow Camo', 'Large')

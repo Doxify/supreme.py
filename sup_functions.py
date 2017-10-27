e3c36c9d00404e1e254d928be01bf4655775a6af
Andrei Georgescu <admin@hydrapvp.net>
2017-10-26 23:38:32 -0700

    Update sup_functions.py
    added comments

--- sup_functions.py
+++ sup_functions.py
@@ -2,15 +2,22 @@ import urllib2
 import json
 from essentials import *
 
+#constant, just the supreme mobile_stock.json url
 mobile_stock_url = 'https://www.supremenewyork.com/mobile_stock.json'
 
+#Session function which has the user agent that allows us to spoof a mobile connection
 def session(url):
     user_agent = 'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko)'
+    #Making the request
     session.request = urllib2.Request(url, headers = {'User-Agent' : user_agent})
         
+#Release info
 def get_release_info():
+    #Makes the connection
     session(mobile_stock_url)
+    #Reads the output
     response = urllib2.urlopen(session.request).read()
+    #Json
     response_json = json.loads(str(response))
     release_week = response_json['release_week']
     release_date = response_json['release_date']
@@ -19,27 +26,38 @@ def get_release_info():
     logger('info','Release week:' + str(release_week))
     logger('info','Release date:' + str(release_date))
     logger('info', 'Last mobile api update:' + str(last_update))
-            
+
+#Returning information needed for checkout when you input
+#a keyword, size, category, and color.
 def get_item_info(keyword, category, color, size):
     session(mobile_stock_url)
     response = urllib2.urlopen(session.request).read()
     response_json = json.loads(str(response))
     logger('info', 'Looking up details for "' + keyword + '" in "' + category + '"')
     
+    #Getting the item from a dictionary of other items
     for x in response_json['products_and_categories'][category]:
+        #If it finds it, it adds it to a variable
         if keyword in x['name'] or keyword == x['name']:
             name = x['name']
             item_id = x['id']
             
+            #This gets more specific details needed for checkout, supreme has different
+            #ids for every color and size
             if item_id != '':
                 session('https://www.supremenewyork.com/shop/' + str(item_id) + '.json')
                 response = urllib2.urlopen(session.request).read()
                 response_json = json.loads(str(response))
                 
+                #Going through all the variants
                 for x in response_json['styles']:
+                    #Checking if the color the user submitted is there, if it is
+                    #it gets the style id
                     if x['name'] == color:
                         style_id = x['id']
                         
+                        #Checking if the size the user submitted is there, if it is
+                        #it gets the size id
                         for x in x['sizes']:
                             if x['name'] == size:
                                 size_id = x['id']
@@ -61,6 +79,6 @@ def get_item_info(keyword, category, color, size):
         else:
             logger('error', 'Keyword not found')
             return
-    
-#keyword, category, color, size                                
+
+#get_item_info(keyword, category, color, size)
 get_item_info('Pullover', 'new', 'Yellow Camo', 'Large')
